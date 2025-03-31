d6e6cceb5800e4047f4bb727db6e9770f8a016f2
Tobias Manser <90106236+tobiasmanser@users.noreply.github.com>
2025-03-31 21:43:18 +0200

    reworked css and views

--- routes/index-routes.ts
+++ routes/index-routes.ts
@@ -6,10 +6,11 @@ const router = express.Router();
 router.get("/", (req: Request, res: Response) => indexController.index(req, res));
 
 router.get("/toggle-theme", (req: Request, res: Response) => {
+    const { return_address } = req.query;
     if (req.session.userSettings) {
         req.session.userSettings.theme = req.session.userSettings.theme === 'light' ? 'dark' : 'light';
     }
-    res.redirect(`/`);
+    res.redirect(<string>return_address || '/');
 });
 
 export const indexRoutes = router;
